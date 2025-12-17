62db3a884ff8ce80490ee531670b9dd28f7608bb
milionhub <jmarin53@alu.ucam.edu>
2025-12-17 12:15:55 +0100

    Introduce code smell for Sonar quality check

--- src/main/java/es/ucam/calidad/App.java
+++ src/main/java/es/ucam/calidad/App.java
@@ -3,8 +3,11 @@ package es.ucam.calidad;
 public class App {
 
     public static int suma(int a, int b) {
-        return a + b;
+        int result = 0;
+        result = a + b;
+        return result;
     }
+
 
     public static void main(String[] args) {
         System.out.println("Quality CI Demo");
